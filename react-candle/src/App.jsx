7ccc3100309148a73161558a9d356c14ba0e8ed2
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2024-02-05 20:28:06 +0000

    Added functionality

--- react-candle/src/App.jsx
+++ react-candle/src/App.jsx
@@ -1,32 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './styles.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState(80);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const makeCandleSmaller = () => {
+    if (height > 10) {
+      setHeight((prev) => prev - 10);
+    }
+  };
+
+  useEffect(() => {
+    if (isButtonClicked) {
+      const intervalId = setInterval(() => {
+        makeCandleSmaller();
+      }, 2000); // 3000ms = 1 second
+
+//      const checkHeight = setInterval(() => {
+  //      if (height <= 100 || !isButtonClicked) {
+    //      clearInterval(intervalId);
+      //    clearInterval(checkHeight);
+        //}
+    //  }, 100);
+
+   
+            return () => {
+         clearInterval(intervalId);
+  //      clearInterval(checkHeight);
+      };
+    }
+  }, [isButtonClicked]);
+
+ 
 
   return (
     <>
       <div className="block">
-      <div>
-        <button>Make candle smaller</button>
-      </div>
-      <div className="block candleContainer">
-        <div className="candle">
-          <div className="flame">
-            <div className="shadows" />
-            <div className="top" />
-            <div className="middle" />
-            <div className="bottom" />
+        <div>
+          <button onClick={() => setIsButtonClicked((prev) => !prev)}>Make candle smaller</button>
+        </div>
+        <div className="block candleContainer">
+          <div className="candle" style={{ height: `${height}%` }}>
+            <div className="flame">
+              <div className="shadows" />
+              <div className="top" />
+              <div className="middle" />
+              <div className="bottom" />
+            </div>
+            <div className="wick" />
+            <div className="wax" style={{ height: `${height}%` }} />
           </div>
-          <div className="wick" />
-          <div className="wax" />
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
